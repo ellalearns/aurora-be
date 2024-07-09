31a2fc5b81c582ef5788782cb103d64481f8732a
ellalearns <ellalearnscopy@gmail.com>
2024-07-09 14:16:35 +0100

    create tasks table

--- models/user_model.py
+++ models/user_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from base_model import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 import uuid
 import datetime
 
@@ -17,3 +18,5 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(String, default=(datetime.datetime.now()).isoformat())
     updated_at = Column(String, default=(datetime.datetime.now()).isoformat())
+
+    tasks = relationship("Task", back_populates="user", cascade="all, delete, delete-orphan")
